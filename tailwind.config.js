28612d6c5f4dc5bf5f3f352de30e485a0d60cad6
Blithe-Chiang <cn18750441251@126.com>
2021-09-04 16:54:19 +0800

    set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/**
+ * @type {import("tailwindcss/tailwind-config").TailwindConfig}
+ */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
